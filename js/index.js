5b0c00eeb61deca855300dbce21ff71e9206e151
Shingo Tomioka <s.tomioka.103@gmail.com>
2017-07-09 13:54:41 +0900

    List items (v-for)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-    message: "Hello, Vue.js!"
+    message: "Hello, Vue.js!",
+    people: [
+      { name: "John", age: 20 },
+      { name: "Bob", age: 25 },
+      { name: "Alice", age: 30 }
+    ],
   }
 })
