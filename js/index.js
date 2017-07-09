0778700ff877659ec70db859ba0cbcd1f732c5bd
Shingo Tomioka <s.tomioka.103@gmail.com>
2017-07-09 14:25:00 +0900

    Add person

--- js/index.js
+++ js/index.js
@@ -7,5 +7,10 @@ new Vue({
       { name: "Bob", age: 25 },
       { name: "Alice", age: 30 }
     ],
+  },
+  methods: {
+    addPerson: function (name, age) {
+      this.people = this.people.concat({ name: name, age: age });
+    }
   }
 })
